b6dbbc4bc0c001f813215784f3e1584ec5cf9433
prathiksh13 <prathiksh15@gmail.com>
2026-04-23 22:37:06 +0530

    Update socket.js

--- frontend-react/src/lib/socket.js
+++ frontend-react/src/lib/socket.js
@@ -1,7 +1,6 @@
 import { io } from "socket.io-client"
 
-const SOCKET_URL = import.meta.env.VITE_NODE_API_URL
-
-export const socket = io(SOCKET_URL, {
+export const socket = io("https://serien-model.onrender.com", {
   transports: ["websocket"],
+  withCredentials: true,
 })
